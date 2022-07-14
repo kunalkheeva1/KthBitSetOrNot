cce07e5d743259cb74ee333244331f2fcc4be938
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 22:22:20 -0700

    added comments for better understanding

--- src/KthBitSetOrNot.java
+++ src/KthBitSetOrNot.java
@@ -1,8 +1,14 @@
 public class KthBitSetOrNot {
-    public boolean kthBitSetOrNot(int n, int k){
-        int bitmask = n<<k;
-        int ans = bitmask & 1;
 
+    // return true or false
+    public boolean kthBitSetOrNot(int n, int k){
+
+        // get a bit mask after shifting the 1 kth times
+        int bitmask = 1<<k;
+
+        // take and operator and if its 1 it will return 1 otherwise 0
+        int ans = bitmask & n;
+        // if ans greater than 0 return true otherwise false
         if(ans>0){
             return true;
         }else{
