b54ea4c12453a64b30c98649a755b559c94a6270
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 22:18:45 -0700

    created a method to check the kth bit is 0 or not

--- src/KthBitSetOrNot.java
+++ src/KthBitSetOrNot.java
@@ -1,5 +1,14 @@
 public class KthBitSetOrNot {
-    
+    public boolean kthBitSetOrNot(int n, int k){
+        int bitmask = n<<k;
+        int ans = bitmask & 1;
+
+        if(ans>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
     public static void main(String[] args) {
 
